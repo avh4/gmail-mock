8fde2d4403f54113f6f976732c5504ffe5386b04
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-11 22:51:52 -0700

    threads.list should not return messages, and implement threads.get

--- src/main/js/gmail.js
+++ src/main/js/gmail.js
@@ -2,7 +2,7 @@ module.exports = function() {
   var threads = [];
   return {
     addMessage: function(message) { 
-      threads.push({ messages: [ message ]});
+      threads.push({ id: message.threadId, messages: [ message ]});
     },
     
     discover: function() {
@@ -19,7 +19,22 @@ module.exports = function() {
                         return {
                           execute: function(callback) {
                             var err;
-                            var res = { threads: threads };
+                            var res = { threads: threads.map(function (t) {
+                              return { id: t.id };
+                            }) };
+                            callback(err, res);
+                          }
+                        }
+                      }
+                    }
+                  },
+                  get: function(id) {
+                    return {
+                      withAuthClient: function() {
+                        return {
+                          execute: function(callback) {
+                            var err;
+                            var res = threads[0];
                             callback(err, res);
                           }
                         }
--- src/test/js/GmailSpec.js
+++ src/test/js/GmailSpec.js
@@ -4,30 +4,57 @@ var MockGmail = require('../../../index');
 describe('fakeGmail', function() {
   var subject;
   var authClient;
+  var client;
   
-  beforeEach(function() {
+  beforeEach(function(done) {
     subject = MockGmail();
     authClient = {};
+    
+    subject.discover('gmail', 'v1').execute(function(err, _client) {
+      client = _client;
+      done();
+    });
   });
   
   it('should return no threads when there are no messages added', function(done) {
-    subject.discover('gmail', 'v1').execute(function(err, client) {
-      client.gmail.users.threads.list({ userId: 'me', q: 'in:inbox' }).withAuthClient(authClient).execute(function(err, res) {
-        expect(res.threads).to.eql([]);
-        done();
-      })
-    })
+    client.gmail.users.threads.list({ userId: 'me', q: 'in:inbox' }).withAuthClient(authClient).execute(function(err, res) {
+      expect(res.threads).to.eql([]);
+      done();
+    });
   });
   
-  it('should return threads for added messages', function(done) {
-    var message = {threadId: 'T1'};
-    subject.addMessage(message);
+  describe('with messages added', function() {
+    beforeEach(function() {
+      var message = {id: 'M1', threadId: 'T1'};
+      subject.addMessage(message);
+    });
+    
+    describe('users.threads.list', function() {
+      var res;
+      
+      beforeEach(function(done) {
+        client.gmail.users.threads.list({ userId: 'me', q: 'in:inbox' }).withAuthClient(authClient).execute(function(err, _res) {
+          res = _res;
+          done();
+        });
+      });
 
-    subject.discover('gmail', 'v1').execute(function(err, client) {
-      client.gmail.users.threads.list({ userId: 'me', q: 'in:inbox' }).withAuthClient(authClient).execute(function(err, res) {
-        expect(res.threads[0].messages[0]).to.eql(message);
-        done();
-      })
+      it('should return threads', function() {
+        expect(res.threads[0].id).to.equal('T1');
+      });
+      
+      it('should not include the messages', function() {
+        expect(res.threads[0].messages).to.be.undefined;
+      });
+    });
+    
+    describe('users.threads.get', function() {
+      it('should return the list of messages', function(done) {
+        client.gmail.users.threads.get({ id: 'T1'}).withAuthClient(authClient).execute(function(err, res) {
+          expect(res.messages[0].id).to.equal('M1');
+          done();
+        });
+      });
     })
   });
 });
